51bfbfa6305b7874677d263b4c05a9772d570997
anjali420 <anjaliamle1@gmail.com>
2019-03-26 23:17:19 +0530

    sacas

---
+++ eclipse-workspace/Selenium/src/Techbodhi/Gmail.java
@@ -0,0 +1,14 @@
+package Techbodhi;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Gmail {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
